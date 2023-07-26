41cb56737256428999e4c45148f03d43156a1f7e
Steve Malmskog <stevem@netskope.com>
2023-07-25 19:43:07 -0700

    feat(queue): convert to use generics and modules

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -4,11 +4,15 @@
 
 package queue
 
-import "testing"
-import "container/list"
-import "math/rand"
+import (
+	"bytes"
+	"container/list"
+	"fmt"
+	"math/rand"
+	"testing"
+)
 
-func ensureEmpty(t *testing.T, q *Queue) {
+func ensureEmpty(t *testing.T, q *Queue[*int]) {
 	if l := q.Len(); l != 0 {
 		t.Errorf("q.Len() = %d, want %d", l, 0)
 	}
@@ -27,116 +31,134 @@ func ensureEmpty(t *testing.T, q *Queue) {
 }
 
 func TestNew(t *testing.T) {
-	q := New()
+	q := New[*int]()
 	ensureEmpty(t, q)
 }
 
-func ensureSingleton(t *testing.T, q *Queue) {
+func ensureSingleton(t *testing.T, q *Queue[*int]) {
 	if l := q.Len(); l != 1 {
 		t.Errorf("q.Len() = %d, want %d", l, 1)
 	}
-	if e := q.Front(); e != 42 {
-		t.Errorf("q.Front() = %v, want %v", e, 42)
+	if e := q.Front(); *e != 42 {
+		t.Errorf("q.Front() = %v, want %v", *e, 42)
 	}
-	if e := q.Back(); e != 42 {
-		t.Errorf("q.Back() = %v, want %v", e, 42)
+	if e := q.Back(); *e != 42 {
+		t.Errorf("q.Back() = %v, want %v", *e, 42)
 	}
 }
 
 func TestSingleton(t *testing.T) {
-	q := New()
+	i := 42
+	q := New[*int]()
 	ensureEmpty(t, q)
-	q.PushFront(42)
+	q.PushFront(&i)
 	ensureSingleton(t, q)
 	q.PopFront()
 	ensureEmpty(t, q)
-	q.PushBack(42)
+	q.PushBack(&i)
 	ensureSingleton(t, q)
 	q.PopBack()
 	ensureEmpty(t, q)
-	q.PushFront(42)
+	q.PushFront(&i)
 	ensureSingleton(t, q)
 	q.PopBack()
 	ensureEmpty(t, q)
-	q.PushBack(42)
+	q.PushBack(&i)
 	ensureSingleton(t, q)
 	q.PopFront()
 	ensureEmpty(t, q)
 }
 
 func TestDuos(t *testing.T) {
-	q := New()
+	i := 42
+	j := 43
+	q := New[*int]()
 	ensureEmpty(t, q)
-	q.PushFront(42)
+	q.PushFront(&i)
 	ensureSingleton(t, q)
-	q.PushBack(43)
+	q.PushBack(&j)
 	if l := q.Len(); l != 2 {
 		t.Errorf("q.Len() = %d, want %d", l, 2)
 	}
-	if e := q.Front(); e != 42 {
-		t.Errorf("q.Front() = %v, want %v", e, 42)
+	if e := q.Front(); *e != 42 {
+		t.Errorf("q.Front() = %v, want %v", *e, 42)
 	}
-	if e := q.Back(); e != 43 {
-		t.Errorf("q.Back() = %v, want %v", e, 43)
+	if e := q.Back(); *e != 43 {
+		t.Errorf("q.Back() = %v, want %v", *e, 43)
 	}
 }
 
-func ensureLength(t *testing.T, q *Queue, len int) {
+func ensureLength(t *testing.T, q *Queue[*int], len int) {
 	if l := q.Len(); l != len {
 		t.Errorf("q.Len() = %d, want %d", l, len)
 	}
 }
 
 func TestZeroValue(t *testing.T) {
-	var q Queue
-	q.PushFront(1)
+	v := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	var q Queue[*int]
+	q.PushFront(&v[0])
 	ensureLength(t, &q, 1)
-	q.PushFront(2)
+	q.PushFront(&v[1])
 	ensureLength(t, &q, 2)
-	q.PushFront(3)
+	q.PushFront(&v[2])
 	ensureLength(t, &q, 3)
-	q.PushFront(4)
+	q.PushFront(&v[3])
 	ensureLength(t, &q, 4)
-	q.PushFront(5)
+	q.PushFront(&v[4])
 	ensureLength(t, &q, 5)
-	q.PushBack(6)
+
+	q.PushBack(&v[5])
 	ensureLength(t, &q, 6)
-	q.PushBack(7)
+	q.PushBack(&v[6])
 	ensureLength(t, &q, 7)
-	q.PushBack(8)
+	q.PushBack(&v[7])
 	ensureLength(t, &q, 8)
-	q.PushBack(9)
+	q.PushBack(&v[8])
 	ensureLength(t, &q, 9)
-	const want = "[5 4 3 2 1 6 7 8 9]"
-	if s := q.String(); s != want {
-		t.Errorf("q.String() = %s, want %s", s, want)
+
+	// Output should be: [5 4 3 2 1 6 7 8 9]
+	wantIdx := []int{4, 3, 2, 1, 0, 5, 6, 7, 8}
+	var want bytes.Buffer
+	want.WriteByte('[')
+	for k, i := range wantIdx {
+		want.WriteString(fmt.Sprintf("%v", &v[i]))
+		if k < len(wantIdx)-1 {
+			want.WriteByte(' ')
+		}
+	}
+	want.WriteByte(']')
+	if s := q.String(); s != want.String() {
+		t.Errorf("q.String() = %s, want %s", s, want.String())
 	}
 }
 
 func TestGrowShrink1(t *testing.T) {
-	var q Queue
+	var q Queue[*int]
 	for i := 0; i < size; i++ {
-		q.PushBack(i)
+		i := i
+		q.PushBack(&i)
 		ensureLength(t, &q, i+1)
 	}
 	for i := 0; q.Len() > 0; i++ {
-		x := q.PopFront().(int)
-		if x != i {
+		x := q.PopFront()
+		if *x != i {
 			t.Errorf("q.PopFront() = %d, want %d", x, i)
 		}
 		ensureLength(t, &q, size-i-1)
 	}
 }
 func TestGrowShrink2(t *testing.T) {
-	var q Queue
+	var q Queue[*int]
 	for i := 0; i < size; i++ {
-		q.PushFront(i)
+		i := i
+		q.PushFront(&i)
 		ensureLength(t, &q, i+1)
 	}
 	for i := 0; q.Len() > 0; i++ {
-		x := q.PopBack().(int)
-		if x != i {
-			t.Errorf("q.PopBack() = %d, want %d", x, i)
+		x := q.PopBack()
+		if *x != i {
+			t.Errorf("q.PopBack() = %d, want %d", *x, i)
 		}
 		ensureLength(t, &q, size-i-1)
 	}
@@ -146,7 +168,7 @@ const size = 1024
 
 func BenchmarkPushFrontQueue(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		var q Queue
+		var q Queue[int]
 		for n := 0; n < size; n++ {
 			q.PushFront(n)
 		}
@@ -163,7 +185,7 @@ func BenchmarkPushFrontList(b *testing.B) {
 
 func BenchmarkPushBackQueue(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		var q Queue
+		var q Queue[int]
 		for n := 0; n < size; n++ {
 			q.PushBack(n)
 		}
@@ -179,7 +201,7 @@ func BenchmarkPushBackList(b *testing.B) {
 }
 func BenchmarkPushBackChannel(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		q := make(chan interface{}, size)
+		q := make(chan int, size)
 		for n := 0; n < size; n++ {
 			q <- n
 		}
@@ -193,16 +215,16 @@ func makeRands() {
 	if rands != nil {
 		return
 	}
-	rand.Seed(64738)
+	rando := rand.New(rand.NewSource(64738))
 	for i := 0; i < 4*size; i++ {
-		rands = append(rands, rand.Float32())
+		rands = append(rands, rando.Float32())
 	}
 }
 func BenchmarkRandomQueue(b *testing.B) {
 	makeRands()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		var q Queue
+		var q Queue[int]
 		for n := 0; n < 4*size; n += 4 {
 			if rands[n] < 0.8 {
 				q.PushBack(n)
@@ -247,7 +269,7 @@ func BenchmarkRandomList(b *testing.B) {
 
 func BenchmarkGrowShrinkQueue(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		var q Queue
+		var q Queue[int]
 		for n := 0; n < size; n++ {
 			q.PushBack(i)
 		}
